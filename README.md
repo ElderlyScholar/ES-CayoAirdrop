ff71f41f80e87a35c6e00d32c57af1ac3bc634e5
ElderlyScholar <pinewoodstaterp@gmail.com>
2024-05-22 13:05:21 +1000

    Add files via upload

---
+++ README.md
@@ -0,0 +1,3 @@
+# Install
+
+Drag Drop Ensure / Make Sure You Set Your Config Up
---
+++ config.lua
@@ -0,0 +1,23 @@
+Config = {}
+
+Config.Debug = false
+Config.DebugCommand = "adloc"
+
+Config.ItemsAmount = 2      -- Amount Of Items You Get From The Crate
+Config.Items = {
+    "weapon_pistol",
+    "weapon_pistol",
+    "weapon_pistol",
+}
+
+Config.RespawnTime = 15         -- In Mins
+
+Config.Locations = {
+    vector3(3915.13, -4699.76, 3.21),
+    vector3(4189.24, -4479.65, 3.15),
+    vector3(4669.1, -4558.65, 21.85),
+    vector3(4813.72, -5021.86, 30.66),
+    vector3(5250.45, -5443.81, 62.7),
+    vector3(5449.72, -5928.11, 16.45),
+    vector3(4637.82, -5113.46, 9.16),
+}
